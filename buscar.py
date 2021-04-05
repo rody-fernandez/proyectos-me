e4d5d86a23e064366aca59811f6245ae0c6f920d
Rody Fernández <rody.fernandez.saa@gmail.com>
2021-04-05 15:22:14 -0400

    Ajuste de try para validad si esta libre o ocupado

--- buscar.py
+++ buscar.py
@@ -2,13 +2,9 @@ import mysql.connector
 estado = 1
 conexion1=mysql.connector.connect(host="localhost", user="root", passwd="", database="python-user")
 cursor1=conexion1.cursor()
-legajo= int(input("Ingrese su legajo: "))
-#cursor1.execute("SELECT name FROM users WHERE id = %s;", (legajo,))
-cursor1.execute("""
-    UPDATE users 
-    SET estado = 1 
-    WHERE id = 2;
-""")
+legajo= int(input("Ingrese su nro: "))
+cursor1.execute("SELECT name FROM users WHERE id = %s;", (legajo,))
+
 #for tabla in cursor1:
-print(legajo)
+print(table)
 #conexion1.close() 
--- conectate.py
+++ conectate.py
@@ -1,19 +1,30 @@
 import mysql.connector
+cerradura = 0
 
-try:
-    connection = mysql.connector.connect(host='localhost', database='python-user', user='root', password='')
-    cursor = connection.cursor()
-    input_data = int(input("Ingrese su legajo: "))
-    sql  = "Update users set estado = %s where id = %s "
-    val = ("2", input_data)
-    cursor.execute(sql, val)
-    #cursor.execute(input_data)
-    connection.commit()
-    print("Record Updated successfully ")
-except mysql.connector.Error as error:
-    print("Failed to update record to database: {}".format(error))
-finally:
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("MySQL connection is closed")
+while True:
+    try:
+        connection = mysql.connector.connect(host='localhosst', database='python-user', user='root', password='')
+        cursor = connection.cursor()
+        if cerradura == 0:
+            legajo = int(input("Ingrese su legajo: "))
+            sql  = "Update users set estado = %s where estado = 1 and id = %s"
+            val = ("0", legajo)
+            cursor.execute(sql, val)
+            connection.commit()
+            print("OCUPADO")
+            cerradura = 1
+        else:
+            legajo = int(input("Ingrese su legajo: "))
+            sql  = "Update users set estado = %s where estado = 0 and id = %s"
+            val = ("1", legajo)
+            cursor.execute(sql, val)
+            connection.commit()
+            print("LIBRE")
+            cerradura = 0
+    except mysql.connector.Error as error:
+        print("Failed to update record to database: {}".format(error))
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
