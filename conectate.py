8a4a4ff04955954e484fdfa9bf6f70a9e8a3ab16
Rody Fernández <rody.fernandez.saa@gmail.com>
2021-04-07 15:15:29 -0400

    Validar cuando esta ocupado

--- conectate.py
+++ conectate.py
@@ -1,28 +1,34 @@
 import mysql.connector
 cerradura = 0
+estado = 0
 
 while True:
     try:
-        connection = mysql.connector.connect(host='localhosst', database='python-user', user='root', password='')
+        connection = mysql.connector.connect(host='localhost', database='python-user', user='root', password='')
         cursor = connection.cursor()
-        if cerradura == 0:
-            legajo = int(input("Ingrese su legajo: "))
-            sql  = "Update users set estado = %s where estado = 1 and id = %s"
-            val = ("0", legajo)
-            cursor.execute(sql, val)
-            connection.commit()
-            print("OCUPADO")
-            cerradura = 1
-        else:
-            legajo = int(input("Ingrese su legajo: "))
+        legajo = int(input("Ingrese su legajo: "))
+        if cerradura == 0 and estado == 0:
             sql  = "Update users set estado = %s where estado = 0 and id = %s"
             val = ("1", legajo)
             cursor.execute(sql, val)
             connection.commit()
-            print("LIBRE")
+            print(f"Bienvenido {legajo}")
+            cerradura = 1
+            estado = int(legajo)
+        elif  cerradura == 1 and estado == legajo:
+            sql  = "Update users set estado = %s where estado = 1 and id = %s"
+            val = ("0", legajo)
+            cursor.execute(sql, val)
+            connection.commit()
+            print(f"Hasta Luego! {legajo}")
             cerradura = 0
+            estado = 0
+        else:
+            if cerradura == 1 and estado != 0:
+                print("Perdon! Esta ocupado")
+                  
     except mysql.connector.Error as error:
-        print("Failed to update record to database: {}".format(error))
+            print("Failed to update record to database: {}".format(error))
     finally:
         if connection.is_connected():
             cursor.close()
